9b2fb9aa5b81461b7fde1d226d34f554e4e538ae
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-26 14:06:18 -0500

    working chatbot

--- simple_chat_bot/main.py
+++ simple_chat_bot/main.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 import random
 import json
+import pickle
 
 
 # data preprocessing
@@ -17,55 +18,62 @@ stemmer = LancasterStemmer()
 with open("intents.json") as file:
     data = json.load(file)
 
+try:
+    with open("data.pickle", 'rb') as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            # tokenize our patterns
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        # tokenize our patterns
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?!@#$%^&.,/"]
+    words = sorted(list(set(words)))
 
-words = [stemmer.stem(w.lower()) for w in words if w not in "?!@#$%^&.,/"]
-words = sorted(list(set(words)))
+    labels = sorted(labels)
 
-labels = sorted(labels)
+    training = []
+    output = []
 
-training = []
-output = []
+    # zero all the one-hot encoding 
+    out_empty = [0 for _ in range(len(labels))]
 
-# zero all the one-hot encoding 
-out_empty = [0 for _ in range(len(labels))]
+    # bag of words
 
-# bag of words
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-for x, doc in enumerate(docs_x):
-    bag = []
+        wrds = [stemmer.stem(w) for w in doc]
 
-    wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
 
-    training.append(bag)
-    output.append(output_row)
+        with open("data.pickle", 'wb') as f:
+            pickle.dump((words, labels, training, output), f)
 
-training = np.array(training)
-output = np.array(output)
+    training = np.array(training)
+    output = np.array(output)
 
 # Neural Network
 ##tf.reset_default_graph()
@@ -80,5 +88,44 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return np.array(bag)
+
+def chat():
+    print("Start talking with the bot! (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+        results = model.predict([bag_of_words(inp, words)])#####[0]  # [0] was added
+        #print(results) # this is the result of the prediction in probability
+
+
+        results_index = np.argmax(results)
+        tag = labels[results_index]
+        #print(tag) # this is the tag that most likely represents the user input
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        print(random.choice(responses))
+
+print("\n\n")
+
+chat()
